62617a4c8265aca8443e271453da55687636f2aa
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-06-20 17:39:04 +0000

    ckjhqrbc43i

---
+++ pw2b2a2/src/main/java/joao.grunenberg/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package joao.grunenberg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index (Model model)
+}
